f37a08554658d67e8f06c781512f6aff8a3cae96
michalu <fortunepick.com@gmail.com>
2013-08-31 04:17:14 +0100

    added web.js

--- web.js
+++ web.js
@@ -8,6 +8,8 @@ app.get('/', function(request, response) {
   response.send(content.toString());
 });
 
+app.use('/public',express.static(__dirname + '/public'));
+
 var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
